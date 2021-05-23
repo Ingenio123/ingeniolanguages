fa1b90761b1670aeca758129feb83b72763660f8
Your Name <you@example.com>
2021-05-23 06:58:28 -0500

    arreglo de class to className part of price section

--- src/components/priceSection/PriceSection.jsx
+++ src/components/priceSection/PriceSection.jsx
@@ -25,12 +25,12 @@ export const PriceSection = ()=>{
                 <div id="cursos" className="container">
                     <div className="row">
                         <div className="col-sm-12 col-md-4">
-                            <div class="card">
-                                <img src="https://cdn.pixabay.com/photo/2017/07/13/03/15/paris-2499022_960_720.jpg" alt="" class="card-img-top"/>
-                                <div class="card-body text-center">
-                                    <span class="h2">$</span><span class="display-1 align-middle ">9</span>
-                                    <p class="Promo">Promo</p>
-                                    <p class="card-text description-cards">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className="card">
+                                <img src="https://cdn.pixabay.com/photo/2017/07/13/03/15/paris-2499022_960_720.jpg" alt="" className="card-img-top"/>
+                                <div className="card-body text-center">
+                                    <span className="h2">$</span><span className="display-1 align-middle ">9</span>
+                                    <p className="Promo">Promo</p>
+                                    <p className="card-text description-cards">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                     <Btnbuy>Buy Now</Btnbuy>
                                 </div>
                             </div>
