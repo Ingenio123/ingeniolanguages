b27958675b9f8117ce7e07e384f7acf7c286d0d7
Your Name <you@example.com>
2021-07-13 18:25:40 -0500

    third commit  to production

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,12 +15,67 @@ export const Footer = () => {
                         <SubItems to='' >Contac Us</SubItems>
                     </Items>
                     <CopyRight> Copyright © 2021 </CopyRight>
+                    
                </Container>
+               
             </SectionFooter>
+            <TerminosCondiciones>
+                        <PoliticaPriv>
+                            <ItemsPoliticas>Contactos</ItemsPoliticas>
+                            <TextPoliticas><b>Email: </b> contacto@ingenioonline.com</TextPoliticas> 
+                            <TextPoliticas><b>Cell Phone:</b> (+593) 939418502 - (+593) 986635410</TextPoliticas> 
+                            <TextPoliticas><b>Direccion: </b> Ecuador - Santo Domingo de los Tsachilas</TextPoliticas>
+                            <br />
+                            <br /> 
+                            <TextPoliticas><b>Copyright © 2021</b></TextPoliticas> 
+                        </PoliticaPriv>
+                        <PoliticaPriv>
+                            <ItemsPoliticas>Politicas  de privacidad</ItemsPoliticas>
+                            <TextPoliticas>Ingenio Languages. Gestiona sus Datos de manera segura, Con responsabilidad de uso, mantenimiento y divulgacion de su informacion colectadas. Por ejemplo tipo de informacion que usted nos porpociona Nombres,Correo Electronico,Numero Telefonico.</TextPoliticas> 
+                        </PoliticaPriv>
+                        <PoliticaPriv>
+                            <ItemsPoliticas>Terminos  y Condiciones</ItemsPoliticas>
+                            <TextPoliticas3> <b>Terminos Comerciales:</b>  La tarifa por los servicios y cualquier otro cargo que pueda incurrir en relación con tu uso del servicio, como los impuestos y las posibles tarifas de transacción, se cobrarán instataneamente a tu método de pago</TextPoliticas3> 
+                        </PoliticaPriv>
+            </TerminosCondiciones>
             
         </>
     );
 }
+
+
+const TerminosCondiciones = styled.div `
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    
+    @media screen and (max-width: 768px){
+        grid-template-columns: repeat(1,1fr);
+    }
+
+    background-color: #314584;
+    width: 100vw;
+`
+const PoliticaPriv = styled.div `
+    
+`
+const ItemsPoliticas = styled.p `
+    margin-top: 5px;
+    color:white;
+    text-align: center;
+`
+const TextPoliticas = styled.p `
+    color:white;
+    font-size:16px;
+    text-align: justify;
+    margin:0 15px;
+`
+
+const TextPoliticas3 = styled.p `
+    color:white;
+    font-size:16px;
+    text-align: justify;
+    margin-right: 40px;
+`
 
 const SectionFooter = styled.footer `
     display:flex;
