61f59afbbf871d990e48e4edb41b34efd27c7145
Ingenio123 <83844889+Ingenio123@users.noreply.github.com>
2022-03-04 18:36:53 -0500

    commit

--- src/components/Header/ModalRequesFreeClass.jsx
+++ src/components/Header/ModalRequesFreeClass.jsx
@@ -395,7 +395,7 @@ export default function ModalRequesFreeClass({
                 </ContentSelectOrganism>
                 <ContentTextArea>
                   <label>My goals and expectations are...</label>
-                  <TextAreaAtom></TextAreaAtom>
+                  <TextAreaAtom {...register("Goals")}></TextAreaAtom>
                 </ContentTextArea>
                 <Content_Center>
                   <ButtonSubmit>
