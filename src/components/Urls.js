234636ae82be2f786ef514d9d1d0ccfd8f2e3fde
lUISZAPATA123 <support@ingenioonline.com>
2022-05-21 14:54:31 -0500

    commit

--- src/components/Urls.js
+++ src/components/Urls.js
@@ -1,8 +1,8 @@
 module.exports = {
   // url: "http://192.168.1.8:4000",
-  // url: "https://www.ingenioapi.com",
-  url: "http://localhost:4000",
-  urlClient: "http://localhost:3000",
-  // urlClient: "https://www.ingeniolanguages.com",
+  url: "https://www.ingenioapi.com",
+  // url: "http://localhost:4000",
+  // urlClient: "http://localhost:3000",
+  urlClient: "https://www.ingeniolanguages.com",
   // asdasda
 };
